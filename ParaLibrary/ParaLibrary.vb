1f6756bf5118e07b29996ec5d8782f13f0aee234
David <Jimbo>
2017-08-04 12:15:05 -0400

    Add project files.

---
+++ ParaLibrary/ParaLibrary.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data.SqlClient
+Public Class ParaLibrary
+    Public Shared Function getConnection(server As String, database As String, uid As String, pwd As String) As SqlConnection
+        Dim connectionString = "Server = " + server + ";" + "Database = " + database + ";" _
+                                + "UID = " + uid + ";" + "PWD = " + pwd
+        Dim Connection As New SqlConnection()
+        Try
+            Connection.ConnectionString = connectionString
+            Connection.Open()
+        Catch ex As Exception
+            logErrorsToDatabase(ex)
+        End Try
+        Return Connection
+    End Function
+
+    Public Shared Sub executeNonQuery(query As String, connection As SqlConnection)
+            Try
+                Dim command As New SqlCommand(query, connection)
+                command.ExecuteNonQuery()
+            Catch ex As Exception
+                logErrorsToDatabase(ex)
+            Finally
+                connection.Close()
+            End Try
+        End Sub
+        Public Shared Function executeQuery(query As String, connection As SqlConnection) As DataTable
+            Dim data As New DataTable()
+            Try
+                Using command As New SqlDataAdapter(query, connection)
+                    command.Fill(data)
+                End Using
+            Catch ex As Exception
+                logErrorsToDatabase(ex)
+            Finally
+                connection.Close()
+            End Try
+            Return data
+        End Function
+
+
+    Public Shared Sub logErrorsToDatabase(ex As Exception)
+        Dim connectionString = "Server = DESKTOP-H5FMBDG\WEBDB; Database =  ParaAuditTrail; UID = sa; pwd = localadmin"
+        Dim errorSqlConnection As New SqlConnection(connectionString)
+        Dim command As New SqlCommand("INSERT INTO  ErrorLog VALUES ('" + ex.Source.Replace("'", "") + "','" + ex.Message.Replace("'", "") + "','" _
+                                            + ex.TargetSite.Name.Replace("'", "") + "')", errorSqlConnection)
+        Try
+            errorSqlConnection.Open()
+            command.ExecuteNonQuery()
+        Catch ex
+            Console.WriteLine(ex.Message)
+        Finally
+            errorSqlConnection.Close()
+        End Try
+    End Sub
+End Class
+
+
---
+++ TestParaLiibrary/TestParaLibrary.vb
@@ -0,0 +1,117 @@
+﻿Imports ParaLibrary.ParaLibrary
+Imports System.Data.SqlClient
+Imports System.Net.Mail
+
+Module TestParaLibrary
+
+    Public Sub sendEmail()
+        Dim client As New SmtpClient("smtp.gmail.com", 587)
+        client.UseDefaultCredentials = False
+        client.EnableSsl = True
+        client.Credentials = New Net.NetworkCredential("david.jimbo26@gmail.com", "Datnikka1!")
+        Dim mail As New MailMessage("david.jimbo26@gmail.com", "drj2926@yahoo.com", "Test Send Email", getHTML())
+        mail.IsBodyHtml = True
+        client.Send(mail)
+    End Sub
+
+    Public Function getHTML() As String
+        Dim htmlString As String = "<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr {
+    background-color: Blue;
+}
+</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+
+</body>
+</html>"
+        Return htmlString
+    End Function
+
+    Sub Main()
+
+        ''LOG TO DATABASE TEST
+        ''Dim ex As New Exception()
+        ''ex.Source = "test"
+        ''logErrorsToDatabase(ex)
+
+        ''Server = MTA2BWYPARAPP14\SQLEXPRESS; Database = ParaAuditTrail; UID = sa; pwd = 123456789"
+        Dim data As New DataTable()
+        Dim server As String = "DESKTOP-H5FMBDG\WEBDB"
+        Dim databaseNonQuery As String = "ParaAuditTrail"
+        Dim databaseQuery As String = "ParaAuditTrail"
+        Dim uid As String = "sa"
+        Dim pwd As String = "localadmin"
+        Dim connectionNonQuery As New SqlConnection()
+        connectionNonQuery = getConnection(server, databaseNonQuery, uid, pwd)
+        Dim connectionQuery As New SqlConnection()
+        connectionQuery = getConnection(server, databaseQuery, uid, pwd)
+
+        sendEmail()
+        ''TEST QUERY
+        ''data = executeQuery("select * from errorlog ", connectionQuery)
+        ''For Each result As DataRow In data.Rows
+        ''Console.WriteLine(result("ErrorSource"))
+        ''Next
+
+
+        ''TEST NonQUERY
+        ''executeNonQuery("Insert into errorlog values('I','am','thebest')", connectionNonQuery)
+
+        ''TEST EMAIL
+    End Sub
+
+End Module
